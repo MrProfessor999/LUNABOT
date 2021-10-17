bac7ddc493e2cb45d4955d6cd1312c188181a360
MrProfessor999 <91714877+MrProfessor999@users.noreply.github.com>
2021-10-17 12:51:54 +0530

    Update acutebot.config.py

--- acutebot.config.py
+++ acutebot.config.py
@@ -1,12 +1,14 @@
-("TOKEN = 2095511325:AAGu4yvniqTs_vZ5s0YzH39jAqPlqshFLes")
-("WORKERS = 8")
-("TMDBAPI = c7745c11")
-("DB_URI = mongodb+srv://50:50@cluster0.hq9ev.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
-("GENIUS = false")
-("SPT_CLIENT_SECRET = a2f86375c76a408792c1e68082627db6")
-("SPT_CLIENT_ID = 4e27e02b549a4532adad9745d2212d14")
-("DEBUG = false")
-("ARLTOKEN = 0b2d659e239b575f9cdac047a027ae88d5487069ab97a82861c4e0b71110072d62a9dcec4d2685481eb8c29604b374eaa5aa423ab494d00fac966e4fc45167f8499f3952eab45df7b72c3c340546ed858b4d60243d35fe9f9f1b5dbcba553302")
-("APP_URL = https://animebot43.herokuapp.com")
-("APIID = 7813081")
-("APIHASH = deccd07c38a5ec9fa0fa6e58790fe292")
+
+TOKEN = 
+WORKERS = 
+TMDBAPI = 
+DB_URI = 
+GENIUS = 
+SPT_CLIENT_SECRET = 
+SPT_CLIENT_ID = 
+DEBUG = 
+ARLTOKEN = 
+APP_URL = 
+APIID = 
+APIHASH = 
+
