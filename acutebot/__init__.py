3e44be8741627c0589e0b8e0d51880550c48e708
MrProfessor999 <91714877+MrProfessor999@users.noreply.github.com>
2021-10-20 17:35:09 +0530

    Update __init__.py

--- acutebot/__init__.py
+++ acutebot/__init__.py
@@ -34,21 +34,7 @@ if ENV:
     APIHASH = os.environ.get("APIHASH")
 
 
-else:
-     import Config
 
-    TOKEN = Config.TOKEN
-    WORKERS = Config.WORKERS
-    TMDBAPI = Config.TMDBAPI
-    DB_URI = Config.DB_URI
-    GENIUS = Config.GENIUS
-    SPT_CLIENT_SECRET = Config.SPT_CLIENT_SECRET
-    SPT_CLIENT_ID = Config.SPT_CLIENT_ID
-    DEBUG = Config.DEBUG
-    ARLTOKEN = Config.ARL
-    APP_URL = Config.APP_URL
-    APIID = Config.APIID
-    APIHASH = Config.APIHASH
 
 
 if DEBUG:
