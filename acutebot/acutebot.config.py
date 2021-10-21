6a997ca53ee4138a0e8cd0d921d23a584da60209
MrProfessor999 <91714877+MrProfessor999@users.noreply.github.com>
2021-10-21 08:52:57 +0530

    Update acutebot.config.py

--- acutebot/acutebot.config.py
+++ acutebot/acutebot.config.py
@@ -12,42 +12,4 @@
     APIHASH = Config.APIHASH
 
 
-if DEBUG:
-    logging.basicConfig(
-        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-        level=logging.DEBUG,
-    )
-else:
-    logging.basicConfig(
-        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-        level=logging.INFO,
-    )
-
-__version__ = "1.1.3-rev09"
-
-DEV_ID = 1329457821
-LOG = logging.getLogger(__name__)
-
-# Check python version:
-if sys.version_info[0] < 3 or sys.version_info[1] < 6:
-    LOG.info("You MUST need to have python version 3.6! shutting down...")
-    sys.exit(1)
-
-
-def typing(func):
-    """Sends typing action while processing func command."""
-
-    @wraps(func)
-    def command_func(update, context, *args, **kwargs):
-        context.bot.send_chat_action(
-            chat_id=update.effective_chat.id, action=ChatAction.TYPING
-        )
-        return func(update, context, *args, **kwargs)
-
-    return command_func
-
-
-# Use HTML treewide;
-defaults = Defaults(parse_mode=ParseMode.HTML)
-updater = Updater(TOKEN, use_context=True, workers=WORKERS, defaults=defaults)
-dp = updater.dispatcher
+=True, workers=WORKERS, defaults=defaults)
