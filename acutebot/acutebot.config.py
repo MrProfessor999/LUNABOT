f112f02900d294710f734bbffef64aff18c5a919
MrProfessor999 <91714877+MrProfessor999@users.noreply.github.com>
2021-10-21 08:01:34 +0530

    Update acutebot.config.py

--- acutebot/acutebot.config.py
+++ acutebot/acutebot.config.py
@@ -11,8 +11,8 @@ APP_ID = int(os.environ.get("APP_ID", ""))
 #Your API Hash from my.telegram.org
 API_HASH = os.environ.get("API_HASH", "")
 
-#Your db channel Id
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", ""))
+#TMDBAPI
+TMDBAPI = int(os.environ.get("TMDBAPI", ""))
 
 #OWNER ID
 OWNER_ID = int(os.environ.get("OWNER_ID", ""))
@@ -23,4 +23,4 @@ DB_URI = os.environ.get("DATABASE_URL", "")
 #force sub channel id, if you want enable force sub
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
 
-TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
+TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "8"))
