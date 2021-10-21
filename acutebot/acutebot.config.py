5f603b991b257568a4c63779477825a993f040d5
MrProfessor999 <91714877+MrProfessor999@users.noreply.github.com>
2021-10-21 08:08:02 +0530

    Update acutebot.config.py

--- acutebot/acutebot.config.py
+++ acutebot/acutebot.config.py
@@ -1,26 +1,53 @@
-import os
-import logging
-from logging.handlers import RotatingFileHandler
+    TOKEN = Config.TOKEN
+    WORKERS = Config.WORKERS
+    TMDBAPI = Config.TMDBAPI
+    DB_URI = Config.DB_URI
+    GENIUS = Config.GENIUS
+    SPT_CLIENT_SECRET = Config.SPT_CLIENT_SECRET
+    SPT_CLIENT_ID = Config.SPT_CLIENT_ID
+    DEBUG = Config.DEBUG
+    ARLTOKEN = Config.ARL
+    APP_URL = Config.APP_URL
+    APIID = Config.APIID
+    APIHASH = Config.APIHASH
 
-#Bot token @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
 
-#Your API ID from my.telegram.org
-APP_ID = int(os.environ.get("APP_ID", ""))
+if DEBUG:
+    logging.basicConfig(
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        level=logging.DEBUG,
+    )
+else:
+    logging.basicConfig(
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        level=logging.INFO,
+    )
 
-#Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "")
+__version__ = "1.1.3-rev09"
 
-#TMDBAPI
-TMDBAPI = int(os.environ.get("TMDBAPI", ""))
+DEV_ID = 1329457821
+LOG = logging.getLogger(__name__)
 
-#OWNER ID
-OWNER_ID = int(os.environ.get("OWNER_ID", ""))
+# Check python version:
+if sys.version_info[0] < 3 or sys.version_info[1] < 6:
+    LOG.info("You MUST need to have python version 3.6! shutting down...")
+    sys.exit(1)
 
-#Database 
-DB_URI = os.environ.get("DATABASE_URL", "")
 
-#force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
+def typing(func):
+    """Sends typing action while processing func command."""
 
-TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "8"))
+    @wraps(func)
+    def command_func(update, context, *args, **kwargs):
+        context.bot.send_chat_action(
+            chat_id=update.effective_chat.id, action=ChatAction.TYPING
+        )
+        return func(update, context, *args, **kwargs)
+
+    return command_func
+
+
+# Use HTML treewide;
+defaults = Defaults(parse_mode=ParseMode.HTML)
+updater = Updater(TOKEN, use_context=True, workers=WORKERS, defaults=defaults)
+dp = updater.dispatcher
