d7167891016bdf3335e6c00910c676c2da341141
MrProfessor999 <91714877+MrProfessor999@users.noreply.github.com>
2021-12-03 16:05:16 +0530

    Update __init__.py

--- acutebot/helpers/database/__init__.py
+++ acutebot/helpers/database/__init__.py
@@ -16,10 +16,6 @@
 
 from acutebot import DB_URI
 
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, scoped_session
-
 def start() -> scoped_session:
     engine = create_engine(DB_URI, client_encoding="utf8")
     BASE.metadata.bind = engine
