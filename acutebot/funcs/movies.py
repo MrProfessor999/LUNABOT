97563218eea5968229ece8317f220add95ec0573
MrProfessor999 <91714877+MrProfessor999@users.noreply.github.com>
2021-12-03 15:40:56 +0530

    Update movies.py

--- acutebot/funcs/movies.py
+++ acutebot/funcs/movies.py
@@ -26,7 +26,7 @@ from telegram.ext import (
 from telegram.ext.dispatcher import run_async
 from telegram import InlineKeyboardMarkup, InlineKeyboardButton, ForceReply
 
-from acutebot import dp, TMDBAPI, typing
+from acutebot import TMDBAPI, typing
 from acutebot.helpers import strings as st
 from acutebot.helpers.parsedata import byname, currency, sort_caps
 from acutebot.helpers.keyboard import keyboard
