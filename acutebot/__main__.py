6787b6067d006ad764a90c938b88b10d13d7be25
MrProfessor999 <91714877+MrProfessor999@users.noreply.github.com>
2021-12-04 15:29:28 +0530

    Update __main__.py

--- acutebot/__main__.py
+++ acutebot/__main__.py
@@ -141,6 +141,14 @@ def back_btn(update, context):
     stuff = Starter(update.effective_user.first_name)
     query.message.edit_caption(caption=stuff.text, reply_markup=stuff.reply_markup)
 
+def main():
+    if SUPPORT_CHAT is not None and isinstance(SUPPORT_CHAT, str):
+        try:
+            dispatcher.bot.sendMessage(f"@{SUPPORT_CHAT}", "I am now online!")
+        except Unauthorized:
+            LOGGER.warning(
+                "Bot isnt able to send message to support_chat, go and check!"
+            )
 
 
     restart_handler = CommandHandler("reboot", restart, filters=Filters.user(DEV_ID))
